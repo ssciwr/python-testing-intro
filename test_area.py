a043b4b86966e41318a7b34b9e01d6d6b499c9f9
Liam Keegan <liam@keegan.ch>
2023-09-22 12:05:36 +0200

    add exception & parametrize examples

--- test_area.py
+++ test_area.py
@@ -8,5 +8,16 @@ def test_area_of_square():
     assert area_of_square(1) == 1
     # for floats, calculations are not exact, so it's not so simple - this test passes (but we got lucky)
     assert area_of_square(0.5) == 0.25
-    # this one would fail without the approx, because 0.2*0.2 gives 0.04000000000000001 and not exactly 0.04
+    # this one would fail without pytest.approx, because 0.2*0.2 gives 0.04000000000000001 and not exactly 0.04
     assert area_of_square(0.2) == pytest.approx(0.04)
+
+
+def test_area_of_square_invalid():
+    with pytest.raises(TypeError):
+        # this asserts that the code inside here raises a TypeError
+        area_of_square("5")
+
+
+@pytest.mark.parametrize("length,area", [(1.0, 1.0), (0.2, 0.04), (1.2, 1.44)])
+def test_area_of_square_floats(length, area):
+    assert area_of_square(length) == pytest.approx(area)
