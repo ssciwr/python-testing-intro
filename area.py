cabec9b6c1ab507a93fa764f5084f451d030f779
Liam Keegan <liam@keegan.ch>
2023-10-09 09:29:52 +0200

    refactor square to call rectangle

--- area.py
+++ area.py
@@ -1,7 +1,5 @@
 def area_of_square(length):
-    if length < 0:
-        raise ValueError("Length cannot be negative")
-    return length * length
+    return area_of_rectangle(length, length)
 
 
 def area_of_rectangle(length, width):
