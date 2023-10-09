29de44131e562055a1d65eabf90b9c9a0b492f4f
Liam Keegan <liam@keegan.ch>
2023-10-09 11:22:19 +0200

    add char count, temp file fixture

--- test_text.py
+++ test_text.py
@@ -1,9 +1,18 @@
-from text import count_lines
+from text import count_lines, count_characters
 import pytest
 
 
-def test_count_lines(tmp_path):
+@pytest.fixture
+def test_file(tmp_path):
     filename = tmp_path / "temp_file.txt"
     with open(filename, "w") as f:
         f.write("hello\nworld")
-    assert count_lines(filename) == 2
+    return filename
+
+
+def test_count_lines(test_file):
+    assert count_lines(test_file) == 2
+
+
+def test_count_characters(test_file):
+    assert count_characters(test_file) == 11
