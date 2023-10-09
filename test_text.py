ff4dc93fd3ed94189ffee0e58451e54f88857ec0
Liam Keegan <liam@keegan.ch>
2023-10-09 12:01:05 +0200

    return function from fixture to make a generator

--- test_text.py
+++ test_text.py
@@ -3,16 +3,24 @@ import pytest
 
 
 @pytest.fixture
-def test_file(tmp_path):
-    filename = tmp_path / "temp_file.txt"
-    with open(filename, "w") as f:
-        f.write("hello\nworld")
-    return filename
+def test_file_generator(tmp_path):
+    def _test_file(n_lines):
+        filename = tmp_path / "temp_file.txt"
+        with open(filename, "w") as f:
+            for _ in range(n_lines):
+                f.write("hello\n")
+        return filename
+
+    return _test_file
 
 
-def test_count_lines(test_file):
-    assert count_lines(test_file) == 2
+@pytest.mark.parametrize("n_lines", [0, 1, 2, 3, 5, 88])
+def test_count_lines(test_file_generator, n_lines):
+    test_file = test_file_generator(n_lines=n_lines)
+    assert count_lines(test_file) == n_lines
 
 
-def test_count_characters(test_file):
-    assert count_characters(test_file) == 11
+@pytest.mark.parametrize("n_lines", [0, 1, 2, 3, 5, 88])
+def test_count_characters(test_file_generator, n_lines):
+    test_file = test_file_generator(n_lines=n_lines)
+    assert count_characters(test_file) == n_lines * 6
